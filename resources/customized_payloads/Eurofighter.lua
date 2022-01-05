8aeebd4b42cff886080040dbce10d4cb896a76ec
NovemberCharlieHotel <97031381+NovemberCharlieHotel@users.noreply.github.com>
2022-01-05 11:26:31 +0100

    Add default loadouts for liberation

--- resources/customized_payloads/Eurofighter.lua
+++ resources/customized_payloads/Eurofighter.lua
@@ -2,6 +2,127 @@ local unitPayloads = {
 	["name"] = "Eurofighter",
 	["payloads"] = {
 		[1] = {
+			["name"] = "CAS",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{irist}",
+					["num"] = 13,
+				},
+				[2] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 12,
+				},
+				[3] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 11,
+				},
+				[4] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 10,
+				},
+				[5] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 9,
+				},
+				[6] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 8,
+				},
+				[7] = {
+					["CLSID"] = "{5335D97A-35A5-4643-9D9B-026C75961E52}",
+					["num"] = 7,
+				},
+				[8] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 5,
+				},
+				[9] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 6,
+				},
+				[10] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 4,
+				},
+				[11] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 3,
+				},
+				[12] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 2,
+				},
+				[13] = {
+					["CLSID"] = "{irist}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+				[2] = 32,
+			},
+		},
+		[2] = {
+			["name"] = "SEAD",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{irist}",
+					["num"] = 13,
+				},
+				[2] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 12,
+				},
+				[3] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 11,
+				},
+				[4] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 10,
+				},
+				[5] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 9,
+				},
+				[6] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 8,
+				},
+				[7] = {
+					["CLSID"] = "{EF_FuelTank_1000L}",
+					["num"] = 7,
+				},
+				[8] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 6,
+				},
+				[9] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 5,
+				},
+				[10] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 4,
+				},
+				[11] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 3,
+				},
+				[12] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 2,
+				},
+				[13] = {
+					["CLSID"] = "{irist}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 29,
+			},
+		},
+		[3] = {
 			["displayName"] = "CAP",
 			["name"] = "CAP",
 			["pylons"] = {
@@ -63,127 +184,6 @@ local unitPayloads = {
 				[2] = 18,
 				[3] = 10,
 				[4] = 19,
-			},
-		},
-		[2] = {
-			["name"] = "CAS",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{irist}",
-					["num"] = 13,
-				},
-				[2] = {
-					["CLSID"] = "{BRU33_2X_GBU-12}",
-					["num"] = 12,
-				},
-				[3] = {
-					["CLSID"] = "{BRU33_2X_GBU-12}",
-					["num"] = 11,
-				},
-				[4] = {
-					["CLSID"] = "{BRU33_2X_GBU-12}",
-					["num"] = 10,
-				},
-				[5] = {
-					["CLSID"] = "{Meteor}",
-					["num"] = 9,
-				},
-				[6] = {
-					["CLSID"] = "{Meteor}",
-					["num"] = 8,
-				},
-				[7] = {
-					["CLSID"] = "{AAQ-28_LEFT}",
-					["num"] = 7,
-				},
-				[8] = {
-					["CLSID"] = "{Meteor}",
-					["num"] = 5,
-				},
-				[9] = {
-					["CLSID"] = "{Meteor}",
-					["num"] = 6,
-				},
-				[10] = {
-					["CLSID"] = "{BRU33_2X_GBU-12}",
-					["num"] = 4,
-				},
-				[11] = {
-					["CLSID"] = "{BRU33_2X_GBU-12}",
-					["num"] = 3,
-				},
-				[12] = {
-					["CLSID"] = "{Meteor}",
-					["num"] = 2,
-				},
-				[13] = {
-					["CLSID"] = "{irist}",
-					["num"] = 1,
-				},
-			},
-			["tasks"] = {
-				[1] = 31,
-				[2] = 32,
-			},
-		},
-		[3] = {
-			["name"] = "SEAD",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{irist}",
-					["num"] = 13,
-				},
-				[2] = {
-					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
-					["num"] = 12,
-				},
-				[3] = {
-					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
-					["num"] = 11,
-				},
-				[4] = {
-					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
-					["num"] = 10,
-				},
-				[5] = {
-					["CLSID"] = "{Meteor}",
-					["num"] = 9,
-				},
-				[6] = {
-					["CLSID"] = "{Meteor}",
-					["num"] = 8,
-				},
-				[7] = {
-					["CLSID"] = "{EF_FuelTank_1000L}",
-					["num"] = 7,
-				},
-				[8] = {
-					["CLSID"] = "{Meteor}",
-					["num"] = 6,
-				},
-				[9] = {
-					["CLSID"] = "{Meteor}",
-					["num"] = 5,
-				},
-				[10] = {
-					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
-					["num"] = 4,
-				},
-				[11] = {
-					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
-					["num"] = 3,
-				},
-				[12] = {
-					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
-					["num"] = 2,
-				},
-				[13] = {
-					["CLSID"] = "{irist}",
-					["num"] = 1,
-				},
-			},
-			["tasks"] = {
-				[1] = 29,
 			},
 		},
 		[4] = {
@@ -248,7 +248,8 @@ local unitPayloads = {
 			},
 		},
 		[5] = {
-			["name"] = "OCA",
+			["displayName"] = "OCA Guided Munitions",
+			["name"] = "OCA Guided Munitions",
 			["pylons"] = {
 				[1] = {
 					["CLSID"] = "{irist}",
@@ -305,6 +306,627 @@ local unitPayloads = {
 			},
 			["tasks"] = {
 				[1] = 34,
+			},
+		},
+		[6] = {
+			["name"] = "Liberation OCA/Runway",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{irist}",
+					["num"] = 13,
+				},
+				[2] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 12,
+				},
+				[3] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 11,
+				},
+				[4] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 10,
+				},
+				[5] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 9,
+				},
+				[6] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 8,
+				},
+				[7] = {
+					["CLSID"] = "{BRU-42_3*Mk-83}",
+					["num"] = 7,
+				},
+				[8] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 6,
+				},
+				[9] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 5,
+				},
+				[10] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 4,
+				},
+				[11] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 3,
+				},
+				[12] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 2,
+				},
+				[13] = {
+					["CLSID"] = "{irist}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 34,
+			},
+		},
+		[7] = {
+			["displayName"] = "Liberation CAS",
+			["name"] = "Liberation CAS",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{irist}",
+					["num"] = 13,
+				},
+				[2] = {
+					["CLSID"] = "{BRU33_2X_GBU-12}",
+					["num"] = 12,
+				},
+				[3] = {
+					["CLSID"] = "{BRU33_2X_GBU-12}",
+					["num"] = 11,
+				},
+				[4] = {
+					["CLSID"] = "{BRU33_2X_GBU-12}",
+					["num"] = 10,
+				},
+				[5] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 9,
+				},
+				[6] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 8,
+				},
+				[7] = {
+					["CLSID"] = "{AAQ-28_LEFT}",
+					["num"] = 7,
+				},
+				[8] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 5,
+				},
+				[9] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 6,
+				},
+				[10] = {
+					["CLSID"] = "{BRU33_2X_GBU-12}",
+					["num"] = 4,
+				},
+				[11] = {
+					["CLSID"] = "{BRU33_2X_GBU-12}",
+					["num"] = 3,
+				},
+				[12] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 2,
+				},
+				[13] = {
+					["CLSID"] = "{irist}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+				[2] = 32,
+			},
+		},
+		[8] = {
+			["displayName"] = "Liberation TARCAP",
+			["name"] = "Liberation TARCAP",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{irist}",
+					["num"] = 13,
+				},
+				[2] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 12,
+				},
+				[3] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 11,
+				},
+				[4] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 10,
+				},
+				[5] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 9,
+				},
+				[6] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 8,
+				},
+				[7] = {
+					["CLSID"] = "{EF_FuelTank_1000L}",
+					["num"] = 7,
+				},
+				[8] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 6,
+				},
+				[9] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 5,
+				},
+				[10] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 4,
+				},
+				[11] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 3,
+				},
+				[12] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 2,
+				},
+				[13] = {
+					["CLSID"] = "{irist}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 11,
+				[2] = 18,
+				[3] = 10,
+				[4] = 19,
+			},
+		},
+		[9] = {
+			["displayName"] = "Liberation BARCAP",
+			["name"] = "Liberation BARCAP",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{irist}",
+					["num"] = 13,
+				},
+				[2] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 12,
+				},
+				[3] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 11,
+				},
+				[4] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 10,
+				},
+				[5] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 9,
+				},
+				[6] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 8,
+				},
+				[7] = {
+					["CLSID"] = "{EF_FuelTank_1000L}",
+					["num"] = 7,
+				},
+				[8] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 6,
+				},
+				[9] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 5,
+				},
+				[10] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 4,
+				},
+				[11] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 3,
+				},
+				[12] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 2,
+				},
+				[13] = {
+					["CLSID"] = "{irist}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 11,
+				[2] = 18,
+				[3] = 10,
+				[4] = 19,
+			},
+		},
+		[10] = {
+			["displayName"] = "Liberation ESCORT",
+			["name"] = "Liberation ESCORT",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{irist}",
+					["num"] = 13,
+				},
+				[2] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 12,
+				},
+				[3] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 11,
+				},
+				[4] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 10,
+				},
+				[5] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 9,
+				},
+				[6] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 8,
+				},
+				[7] = {
+					["CLSID"] = "{EF_FuelTank_1000L}",
+					["num"] = 7,
+				},
+				[8] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 6,
+				},
+				[9] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 5,
+				},
+				[10] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 4,
+				},
+				[11] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 3,
+				},
+				[12] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 2,
+				},
+				[13] = {
+					["CLSID"] = "{irist}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 11,
+				[2] = 18,
+				[3] = 10,
+				[4] = 19,
+			},
+		},
+		[11] = {
+			["displayName"] = "Liberation DEAD",
+			["name"] = "Liberation DEAD",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{irist}",
+					["num"] = 13,
+				},
+				[2] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 12,
+				},
+				[3] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 11,
+				},
+				[4] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 10,
+				},
+				[5] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 9,
+				},
+				[6] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 8,
+				},
+				[7] = {
+					["CLSID"] = "{CBU-87}",
+					["num"] = 7,
+				},
+				[8] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 6,
+				},
+				[9] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 5,
+				},
+				[10] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 4,
+				},
+				[11] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 3,
+				},
+				[12] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 2,
+				},
+				[13] = {
+					["CLSID"] = "{irist}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 29,
+			},
+		},
+		[12] = {
+			["displayName"] = "Liberation SEAD",
+			["name"] = "Liberation SEAD",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{irist}",
+					["num"] = 13,
+				},
+				[2] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 12,
+				},
+				[3] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 11,
+				},
+				[4] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 10,
+				},
+				[5] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 9,
+				},
+				[6] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 8,
+				},
+				[7] = {
+					["CLSID"] = "{EF_FuelTank_1000L}",
+					["num"] = 7,
+				},
+				[8] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 6,
+				},
+				[9] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 5,
+				},
+				[10] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 4,
+				},
+				[11] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 3,
+				},
+				[12] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 2,
+				},
+				[13] = {
+					["CLSID"] = "{irist}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 29,
+			},
+		},
+		[13] = {
+			["displayName"] = "Liberation OCA/Aircraft",
+			["name"] = "Liberation OCA/Aircraft",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{irist}",
+					["num"] = 13,
+				},
+				[2] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 12,
+				},
+				[3] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 11,
+				},
+				[4] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 10,
+				},
+				[5] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 9,
+				},
+				[6] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 8,
+				},
+				[7] = {
+					["CLSID"] = "{5335D97A-35A5-4643-9D9B-026C75961E52}",
+					["num"] = 7,
+				},
+				[8] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 5,
+				},
+				[9] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 6,
+				},
+				[10] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 4,
+				},
+				[11] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 3,
+				},
+				[12] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 2,
+				},
+				[13] = {
+					["CLSID"] = "{irist}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+				[2] = 32,
+			},
+		},
+		[14] = {
+			["displayName"] = "Liberation Strike",
+			["name"] = "Liberation Strike",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{irist}",
+					["num"] = 13,
+				},
+				[2] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 12,
+				},
+				[3] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 11,
+				},
+				[4] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 10,
+				},
+				[5] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 9,
+				},
+				[6] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 8,
+				},
+				[7] = {
+					["CLSID"] = "{5335D97A-35A5-4643-9D9B-026C75961E52}",
+					["num"] = 7,
+				},
+				[8] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 5,
+				},
+				[9] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 6,
+				},
+				[10] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 4,
+				},
+				[11] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 3,
+				},
+				[12] = {
+					["CLSID"] = "{BRU33_2X_MK-83}",
+					["num"] = 2,
+				},
+				[13] = {
+					["CLSID"] = "{irist}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+				[2] = 32,
+			},
+		},
+		[15] = {
+			["displayName"] = "Liberation SEAD Escort",
+			["name"] = "Liberation SEAD Escort",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{irist}",
+					["num"] = 13,
+				},
+				[2] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 12,
+				},
+				[3] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 11,
+				},
+				[4] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 10,
+				},
+				[5] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 9,
+				},
+				[6] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 8,
+				},
+				[7] = {
+					["CLSID"] = "{EF_FuelTank_1000L}",
+					["num"] = 7,
+				},
+				[8] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 6,
+				},
+				[9] = {
+					["CLSID"] = "{Meteor}",
+					["num"] = 5,
+				},
+				[10] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 4,
+				},
+				[11] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 3,
+				},
+				[12] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C9C}",
+					["num"] = 2,
+				},
+				[13] = {
+					["CLSID"] = "{irist}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 29,
 			},
 		},
 	},
